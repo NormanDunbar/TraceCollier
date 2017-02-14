a630d371775e2cf79d9666671b9163f3b55909b6
Norman Dunbar <norman@vbCentos.casfs.co.uk>
2017-02-14 10:55:18 +0000

    Make_pdf.sh amended to allow older versions of pandoc to be used. Some parameters not accepted on RHEL7, OEL7, CentOS7 etc.

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,8 +1,42 @@
-#! /usr/env bash
+#! /usr/bin/env bash
 #---------------------------------------------------------------
 # Builds a README.pdf from the README.md source file.
 # Assumes pandoc is installed and on the PATH.
 # Norman Dunbar.
 # February 2017.
 #---------------------------------------------------------------
-pandoc --from markdown --to latex --toc --toc-depth=3 --output README.pdf --variable linkcolor=Gray --variable urlcolor=Gray --variable toccolor=Gray --variable margin-left=3cm --variable margin-right=3cm --variable margin-top=3cm --variable margin-bottom=4cm README.md
+# May not work on RHEL, CENTOS 7, Oracle Linux 7 etc. Even if
+# pandoc is installed as it installs version 1.12.3.1. Too old!
+# I *think* version 1.17 is the minimum that uses the xcolor
+# LaTeX package. 
+#---------------------------------------------------------------
+PANDOC=`pandoc --version | head -1 | cut -d" " -f2`
+PANDOC_MINOR=`echo ${PANDOC} | cut -d"." -f2`
+PANDOC_COLOUR="Gray"
+PANDOC_PDF="README.pdf"
+PANDOC_TOC="${PANDOC_COLOUR}"
+
+echo "Pandoc version ${PANDOC} in use."
+
+if [ "${PANDOC_MINOR}" -lt "17" ]
+then
+    ## Pandoc too old, can't cope.
+    PANDOC_COLOUR="blue"
+    PANDOC_TOC="black"
+    echo "Pandoc version ${PANDOC} is too old."
+else
+    echo "Pandoc version ${PANDOC} is recent."
+fi
+
+echo "URLs and internal links will be ${PANDOC_COLOUR}."
+echo "Table of contents links will be ${PANDOC_TOC}."
+
+pandoc --from markdown --to latex --toc --toc-depth=3 --output "${PANDOC_PDF}" --variable linkcolor="${PANDOC_COLOUR}" --variable urlcolor="${PANDOC_COLOUR}" --variable toccolor="${PANDOC_COLOUR}" --variable margin-left=3cm --variable margin-right=3cm --variable margin-top=3cm --variable margin-bottom=4cm README.md
+
+if [ "${?}" -eq "0" ]
+then
+    echo "${PANDOC_PDF}" created.
+else
+    echo "${PANDOC_PDF}" failed to create.
+fi
+
