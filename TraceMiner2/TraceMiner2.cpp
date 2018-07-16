f369d9c36ea8d1af398f8acca33353b5e98466d5
Norman Dunbar <norman@dunbar-it.co.uk>
2018-07-16 13:21:24 +0100

    Version upgraded to 0.08.

--- TraceMiner2/TraceMiner2.cpp
+++ TraceMiner2/TraceMiner2.cpp
@@ -151,7 +151,7 @@
 #include "favicon.h"
 
 // Version number.
-const float version = 0.07;
+const float version = 0.08;
 
 // Various flags set according to the passed parameters.
 tmOptions options;
